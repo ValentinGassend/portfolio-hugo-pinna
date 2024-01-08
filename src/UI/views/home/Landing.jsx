e8bb8aeb8edc5cbc3aa3a2135379715deede1a7c
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-01-08 17:35:25 +0100

    grid enable

--- src/UI/views/home/Landing.jsx
+++ src/UI/views/home/Landing.jsx
@@ -12,7 +12,43 @@ const Landing = () => {
 
     return (
         <>
-            <div style={{backgroundColor: "black", width: "100%", height: "100%"}}></div>
+            <div className={`Landing`}>
+                <div className={`Landing-grid`}>
+                    <div className={`Landing-grid-name`}>
+                        <span className={`Landing-grid--element uppercase`}>Hugo</span>
+                        <span className={`Landing-grid--element uppercase`}>Pinna</span>
+                        <span className={`Landing-grid--element uppercase`}>Cuillère</span>
+                    </div>
+                    <div className={`Landing-grid-domain`}>
+                        <span className={`Landing-grid--element uppercase`}>Art director</span>
+                        <span className={`Landing-grid--element uppercase`}>Graphic designer</span>
+                        <span className={`Landing-grid--element uppercase`}>3d artist</span>
+                    </div>
+                    <div className={`Landing-grid-location`}>
+                        <span className={`Landing-grid--element uppercase`}>Genève</span>
+                        <span className={`Landing-grid--element`}>[[arrow]]</span>
+                        <span className={`Landing-grid--element uppercase`}>Annecy</span>
+                    </div>
+                    <div className={`Landing-grid-baseline`}>
+                        <span className={`Landing-grid--element uppercase`}>“Baseline un peu cool”</span>
+                    </div>
+                    <div className={`Landing-grid-support`}>
+                        <span className={`Landing-grid--element`}>Portfolio</span>
+                    </div>
+                    <div className={`Landing-grid-instagram`}>
+                        <span className={`Landing-grid--element`}>@hugocuillère</span>
+                    </div>
+                    <div className={`Landing-grid-work`}>
+                    <span className={`Landing-grid--element uppercase`}>[[GreenDot]]Open to work</span>
+                    <span className={`Landing-grid--element`}>[[arrow]]</span>
+                    <span className={`Landing-grid--element`}>hugopinna@free.fr</span>
+                    </div>
+                    <div className={`Landing-grid-status`}>
+                    <span className={`Landing-grid--element uppercase`}>Freelance</span>
+                    <span className={`Landing-grid--element uppercase`}>{new Date().getFullYear()}</span>
+                    </div>
+                </div>
+            </div>
             <Loader isPageReady={isPageReady}/>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
+import Landing from "./UI/views/home/Landing.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([{
+    path: "/", element: (<Landing/>),
+}, {
+    path: "about", element: <div>About</div>,
+},]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>,)
