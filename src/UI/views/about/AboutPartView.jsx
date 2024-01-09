11a59e3023b2f71385be1525260e8c45096cf299
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-01-09 14:21:37 +0100

    about part introduction

---
+++ src/UI/views/about/AboutPartView.jsx
@@ -0,0 +1,16 @@
+import PartTitle from "../../components/PartTitle.jsx";
+
+const ProjectsPartView = (ProjectData) => {
+
+
+    return (
+        <section className={`About`} id={`about`}>
+            <PartTitle ParentClass={"About"} titleText={"About"}/>
+
+
+
+
+        </section>
+    )
+}
+export default ProjectsPartView
