4b722c4d365e0a0651c09d77d6c914c7cd2d974a
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-01-25 16:40:13 +0100

    add content about

---
+++ src/UI/views/about/AboutContentView.jsx
@@ -0,0 +1,14 @@
+const AboutContentView = () => {
+
+    return (
+        <div className={`About-content`}>
+            <img className={`About-content--img`} src={'url'} alt={'image de la page contenu'}/>
+            <p className={`About-content--text`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+    )
+}
+
+export default AboutContentView
--- src/UI/views/about/AboutPartView.jsx
+++ src/UI/views/about/AboutPartView.jsx
@@ -1,16 +1,17 @@
 import PartTitle from "../../components/PartTitle.jsx";
+import AboutContentView from "./AboutContentView.jsx";
 
-const ProjectsPartView = (ProjectData) => {
+const AboutPartView = () => {
 
 
     return (
         <section className={`About`} id={`about`}>
             <PartTitle ParentClass={"About"} titleText={"About"}/>
-
+            <AboutContentView/>
 
 
 
         </section>
     )
 }
-export default ProjectsPartView
+export default AboutPartView
