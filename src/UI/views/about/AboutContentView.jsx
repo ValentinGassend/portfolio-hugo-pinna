c46adc7224103aa35d27bd9471a032d03c64e88d
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-02-13 00:14:59 +0100

    modifing url of about

--- src/UI/views/about/AboutContentView.jsx
+++ src/UI/views/about/AboutContentView.jsx
@@ -2,7 +2,7 @@ const AboutContentView = () => {
 
     return (
         <div className={`About-content`}>
-            <img className={`About-content--img`} src={'url'} alt={'image de la page contenu'}/>
+            <img className={`About-content--img`} src={'https://picsum.photos/200/300'} alt={'image de la page contenu'}/>
             <p className={`About-content--text`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                 ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
