a019d2537aa110690f5124d65483cd204676c5fc
ValentinGassend <valentin.gassend@gmail.com>
2024-02-13 12:51:54 +0100

    moving to homePage

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
-import Home from "./UI/views/home/Home.jsx";
+import Home from "./UI/views/homePage/Home.jsx";
 
 const router = createBrowserRouter([{
     path: "/", element: (<Home/>),
 }, {
-    path: "about", element: <div>About</div>,
+    path: "/about", element: <div>About</div>,
 },]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>,)
