5456404530d5c38d5d03fdbf9599aa3e229645a9
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-03-12 22:17:17 +0100

    scroll working

--- src/UI/views/homePage/PanelsContainter.jsx
+++ src/UI/views/homePage/PanelsContainter.jsx
@@ -11,7 +11,7 @@ const PanelsContainer = ({isPageReady}) => {
 
     const [panels, setPanels] = useState([]);
 
-    const goToSection = (i, force) => {
+    const goToSection = (i, force, direction) => {
 
         var supportsPassive = false;
         var keys = {37: 1, 38: 1, 39: 1, 40: 1};
@@ -49,30 +49,46 @@ const PanelsContainer = ({isPageReady}) => {
 
 
         if (document.getElementsByClassName("PanelsContainer").length > 0) {
-            let elm, scrollX, scrollY, newX, newY, direction, target;
+            let nextElm, currentElm, scrollX, scrollY, newX, newY, nextTarget, currentTarget;
             /* stash current Window Scroll */
             scrollX = window.pageXOffset;
             scrollY = window.pageYOffset;
             /* scroll to element */
-            window.scrollTo(0,snapTriggers.current[i].trigger.offsetTop);
+            window.scrollTo(0, snapTriggers.current[i].trigger.offsetTop);
             /* calculate new relative element coordinates */
             newX = 0 - window.pageXOffset;
             newY = snapTriggers.current[i].trigger.offsetTop - window.pageYOffset;
             /* grab the element */
-            elm = document.elementFromPoint(newX,newY);
+            nextElm = document.elementFromPoint(newX, newY);
             /* revert to the previous scroll location */
-            window.scrollTo(scrollX,scrollY);
-            console.log("now", elm)
+            window.scrollTo(scrollX, scrollY);
+            console.log(window)
+            if (direction > 0) {
 
-            if (snapTriggers.current[i].direction) {
-                direction = snapTriggers.current[i].direction
+                currentElm = document.elementFromPoint(0, 0);
+            } else {
+                currentElm = document.elementFromPoint(0, window.innerHeight - 1);
+
             }
-                if (elm.classList.contains('pin-spacer')) {
-                    // target the children
-                    target = elm.children[0];
-                } else {
-                    target = elm
-                }
+            console.log("next", nextElm)
+            console.log("current", currentElm)
+            console.log("snapTriggers.current[i].direction", snapTriggers.current[i].direction)
+            console.log("direction", direction)
+
+
+            if (nextElm.classList.contains('pin-spacer')) {
+                // nextTarget the children
+                nextTarget = nextElm.children[0];
+            } else {
+                nextTarget = nextElm
+            }
+
+            if (currentElm.classList.contains('pin-spacer')) {
+                // nextTarget the children
+                currentTarget = currentElm.children[0];
+            } else {
+                currentTarget = currentElm
+            }
             if (i === panels.length - 1 && force) {
                 // User has scrolled to the bottom, smoothly scroll to the first panel without duration
                 gsap.to(window, {
@@ -95,44 +111,73 @@ const PanelsContainer = ({isPageReady}) => {
                 });
 
             } else if (i === 0 && force) {
+                console.log("i === 0 && force")
                 // User has scrolled to the bottom, smoothly scroll to the first panel without duration
                 gsap.to(window, {
                     scrollTo: {y: 1, autoKill: true}, duration: 0, onStart: disableScroll, onComplete: () => {
                         scrollTween.current = null
-                        // if (target.classList.contains('EnterSmoothScroll')) {
-                            scrollTween.current = gsap.to(window, {
-                                scrollTo: {y: snapTriggers.current[i].start, autoKill: true},
-                                duration: 1,
-                                onComplete: () => {
-                                    scrollTween.current = null;
-                                    enableScroll
+                        // if (nextTarget.classList.contains('EnterSmoothScroll')) {
+                        scrollTween.current = gsap.to(window, {
+                            scrollTo: {y: snapTriggers.current[i].start, autoKill: true},
+                            duration: 1,
+                            onComplete: () => {
+                                scrollTween.current = null;
+                                enableScroll
 
-                                },
-                                overwrite: true,
-                            });
+                            },
+                            overwrite: true,
+                        });
                         // }
                     }, overwrite: true,
                 });
             } else {
                 // Scroll to the specified panel with a duration
-                if (target.classList.contains('EnterSmoothScroll')) {
+                if (direction > 0) {
+                    if (nextTarget.classList.contains('EnterSmoothScroll')) {
 
-                    scrollTween.current = gsap.to(window, {
-                        scrollTo: {
-                            y: snapTriggers.current[i].start,
-                            autoKill: false,
-                            onStart: disableScroll,
-                            onComplete: () => {
-                                // console.log(document.getElementsByClassName("PanelsContainer"))
-                                if (document.getElementsByClassName("PanelsContainer").length <= 0) {
+                        scrollTween.current = gsap.to(window, {
+                            scrollTo: {
+                                y: snapTriggers.current[i].start,
+                                autoKill: false,
+                                onStart: disableScroll,
+                                onComplete: () => {
+                                    // console.log(document.getElementsByClassName("PanelsContainer"))
+                                    if (document.getElementsByClassName("PanelsContainer").length <= 0) {
 
-                                    snapTriggers.current = null
+                                        snapTriggers.current = null
+                                    }
+                                    enableScroll
+
                                 }
-                                enableScroll
+                            },
+                            duration: force ? 0 : 1,
+                            onComplete: () => (scrollTween.current = null),
+                            overwrite: false,
+                        });
+                    }
+                } else {
+                    if (currentTarget.classList.contains('EnterSmoothScroll')) {
 
-                            }
-                        }, duration: force ? 0 : 1, onComplete: () => (scrollTween.current = null), overwrite: false,
-                    });
+                        scrollTween.current = gsap.to(window, {
+                            scrollTo: {
+                                y: snapTriggers.current[i].start,
+                                autoKill: false,
+                                onStart: disableScroll,
+                                onComplete: () => {
+                                    // console.log(document.getElementsByClassName("PanelsContainer"))
+                                    if (document.getElementsByClassName("PanelsContainer").length <= 0) {
+
+                                        snapTriggers.current = null
+                                    }
+                                    enableScroll
+
+                                }
+                            },
+                            duration: force ? 0 : 1,
+                            onComplete: () => (scrollTween.current = null),
+                            overwrite: false,
+                        });
+                    }
                 }
 
             }
@@ -176,23 +221,25 @@ const PanelsContainer = ({isPageReady}) => {
 
             // si self.event.target à la class "smoothScrollToMe" alors scroll d'une traite vers lui de facon smooth sinon laisse moi scroll normalement
 
-            if (!scrollTween.current) {
-                const scrollY = self.scrollY();
-                const windowHeight = window.innerHeight;
-                const documentHeight = document.body.scrollHeight;
-                const bottomOffset = documentHeight - (scrollY + windowHeight);
-                const deltaY = self.deltaY;
-                const scroll = snapScroll(scrollY + deltaY, deltaY > 0 ? 1 : -1);
-                console.log(snapTriggers.current)
-                console.log(scrollStarts.indexOf(scroll))
-                if (scrollY === 0 && deltaY < 0 && scroll !== undefined) {
-                    goToSection(snapTriggers.current.length - 1, true); // Go to the last panel
-                } else if (bottomOffset <= 0 && deltaY > 0) {
-                    // Bottom of the window hits the bottom of the website
-                    goToSection(0, true); // Go to the first panel
-                } else {
-                    goToSection(scrollStarts.indexOf(scroll), false);
-                }
+            const scrollY = self.scrollY();
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.body.scrollHeight;
+            const bottomOffset = documentHeight - (scrollY + windowHeight);
+            const deltaY = self.deltaY;
+            const scroll = snapScroll(scrollY + deltaY, deltaY > 0 ? 1 : -1);
+            console.log(snapTriggers.current)
+            console.log(snapTriggers.current)
+            console.log("windowHeight", windowHeight)
+            console.log("documentHeight", documentHeight)
+            console.log("bottomOffset", bottomOffset)
+            console.log("deltaY", deltaY)
+            if (scrollY === 0 && deltaY < 0 && scroll !== undefined) {
+                goToSection(snapTriggers.current.length - 1, true, deltaY); // Go to the last panel
+            } else if (bottomOffset <= 0 && deltaY > 0) {
+                // Bottom of the window hits the bottom of the website
+                goToSection(0, true, deltaY); // Go to the first panel
+            } else {
+                goToSection(scrollStarts.indexOf(scroll), false, deltaY);
             }
         };
 
