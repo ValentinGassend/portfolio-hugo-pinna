c7e6884e4628f58d050dcacaacc6797be9f63918
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-03-13 21:38:42 +0100

    add videos to projects

--- src/UI/views/projects/ProjectPromoteView.jsx
+++ src/UI/views/projects/ProjectPromoteView.jsx
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 
 const ProjectPromoteView = ({index, project, manager}) => {
     const [imageUrl, setImageUrl] = useState(null);
+    const [mediaType, setMediaType] = useState("image"); // Default media type is image
 
 
     useLayoutEffect(() => {
@@ -28,16 +29,47 @@ const ProjectPromoteView = ({index, project, manager}) => {
                 if (url) {
                     //console.log("URL de l'image:", url);
                     setImageUrl(url);
+                    setMediaType(getMediaType(url));
                 } else {
                     //console.log("L'image n'existe pas ou une erreur s'est produite.");
                 }
             })
             .catch((error) => console.error("Erreur générale:", error));
     }, [project.header_image]);
+
+    const getMediaType = (url) => {
+        // Using regular expression to extract file extension
+        const extensionMatch = url.match(/\.([^.?#]+)(?:[?#]|$)/i);
+
+        // Checking if a valid extension is found
+        if (extensionMatch && extensionMatch[1]) {
+            const extension = extensionMatch[1].toLowerCase();
+
+            // Logging for debugging purposes
+            console.log('Extension:', extension);
+            console.log('Original URL:', url);
+
+            // Checking if the extension corresponds to a video format
+            if (extension === 'mp4' || extension === 'mov' || extension === 'avi' || extension === 'wmv') {
+                return 'video';
+            } else {
+                return 'image';
+            }
+        } else {
+            // If no extension is found, default to 'image'
+            return 'image';
+        }
+    };
     return (<div className={"Projects-promote-item"}>
         <Link refresh="true" data-id={project.id} className={`Projects-promote-card`} to={'/project/' + project.id}>
-            <img className={`Projects-promote-card--img`} src={`${imageUrl}`}
-                 alt={`image d'illustration du projet ${project.name}`}></img>
+            {mediaType === 'image' ? (
+                <img className={`Projects-promote-card--img`} src={`${imageUrl}`} alt={`Illustration of ${project.name}`} />
+            ) : (
+                <video className={`Projects-promote-card--video`} autoPlay loop muted>
+                    <source src={`${imageUrl}`} type={`video/${mediaType === 'mp4' ? 'mp4' : 'ogg'}`} />
+                    Your browser does not support the video tag.
+                </video>
+            )}
             <div className={`Projects-promote-card-content`}>
                 <h2 className={`Projects-promote-card-content--date`}>{project.year}</h2>
                 <h1 className={`Projects-promote-card-content--title`}>{project.name}</h1>
--- src/UI/views/projects/ProjectsPromoteView.jsx
+++ src/UI/views/projects/ProjectsPromoteView.jsx
@@ -1,8 +1,11 @@
 import ProjectPromoteView from "./ProjectPromoteView.jsx";
 
 const ProjectsPromoteView = ({projectsPromoted, manager}) => {
+    console.log(projectsPromoted)
+
     return (<div className={`Projects-promote EnterSmoothScroll`}  style={{ '--PromotedProjectLength': projectsPromoted.length }}>
         {projectsPromoted.map((project, index) => (
+
             <ProjectPromoteView key={project.id} index={index} project={project} manager={manager}/>))}
     </div>)
 }
